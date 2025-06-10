75ee2ae9d17ce0bd9d341a417f946dfa17805331
Rajesh Kumar <rajeshkumar7.dev@gmail.com>
2025-06-10 11:35:16 +0530

    adding attachments optional

--- src/app/mail/mail.service.ts
+++ src/app/mail/mail.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { config } from 'dotenv';
-
+import { Attachment } from 'nodemailer/lib/mailer'; 
 config({ path: '.env' });
 
 @Injectable()
 export class MailService {
-  constructor(private mailer: MailerService) { }
+  constructor(private mailer: MailerService) {}
 
   async sendContactEmail(data) {
-    const buffer = Buffer.from(data.id_proof, 'base64');
+    const attachments: Attachment[] = [];
 
-    await this.mailer.sendMail({
-      to: process.env.TO_MAIL,
-      subject: `Neuspatial Contact Us Form Submission from ${data.first_name} ${data.last_name}`,
-      text: `Hello Team,
+    if (data.id_proof) {
+      try {
+        const buffer = Buffer.from(data.id_proof, 'base64');
+        attachments.push({
+          filename: 'id_proof.png',
+          content: buffer,
+        });
+      } catch (error) {
+        console.error('Invalid base64 ID proof:', error);
+      }
+    }
+
+    const messageBody = `Hello Team,
 
 You have received a new submission from the Neuspatial Contact Us form.
 
@@ -25,18 +34,18 @@ Email: ${data.email}
 Mobile: ${data.mobile}
 Type: ${data.type}
 About: ${data.about}
-ID Proof attach in the attachment
+Id Proof attached:${data.id_proof ? 'ID Proof is attached.' : 'No ID Proof provided.'}
 
 Please follow up with the user at your earliest convenience.
 
 Best regards,
-Neuspatial Web Team`,
-      attachments: [
-        {
-          filename: 'id_proof.png',
-          content: buffer,
-        },
-      ],
+Neuspatial Web Team`;
+
+    await this.mailer.sendMail({
+      to: process.env.TO_MAIL,
+      subject: `Neuspatial Contact Us Form Submission from ${data.first_name} ${data.last_name}`,
+      text: messageBody,
+      attachments, 
     });
   }
 }
